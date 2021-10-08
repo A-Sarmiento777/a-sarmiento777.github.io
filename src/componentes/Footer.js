ef237414d7626f63e61705072668ae6b091b85c0
Alex Sarmiento <sarmiento.alex777@gmail.com>
2021-10-08 13:28:05 +0200

    footer

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -4,7 +4,12 @@ const Footer = () => {
 	return (
         <footer className="bg-light text-center text-lg-start mt-auto" >
             <div className="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
-                © 2021 Copyright:
+                © 2021 Copyright
+                <p></p>
+                Designed by : 
+                <p></p>
+                Jose Cobas -
+                Alex Sarmiento
             </div>
         </footer>
 	);
